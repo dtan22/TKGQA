f21b18ca5e5e773ac0d7987a10299389dcd76935
Dương Thị An <94700478+dtan22@users.noreply.github.com>
2024-06-24 15:54:17 +0700

    updata qa_dataset_ctrn

--- qa_datasets.py
+++ qa_datasets.py
@@ -37,8 +37,8 @@ class QA_Dataset(Dataset):
         questions = pickle.load(open(filename, 'rb'))
         
         #probably change for bert/roberta?
-        self.tokenizer_class = GPT2Tokenizer
-        self.tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+        self.tokenizer_class = DistilBertTokenizer 
+        self.tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
         
         self.all_dicts = utils.getAllDicts(dataset_name)
         print('Total questions = ', len(questions))
@@ -686,7 +686,294 @@ class QA_Dataset_TempoQR(QA_Dataset):
         answers_single = torch.from_numpy(np.array([item[10] for item in items]))
 
         return input_ids, attention_mask, entity_time_ids_padded, entity_mask_padded, heads, tails, times, start_times, end_times, tails2, answers_single
-    
+
+class QA_Dataset_CTRN(QA_Dataset):
+    def __init__(self, split, dataset_name, args, tokenization_needed=True):
+        super().__init__(split, dataset_name, tokenization_needed)
+        print('Preparing data for split %s' % split)
+        ents = self.all_dicts['ent2id'].keys()
+        self.all_dicts['tsstr2id'] = {str(k[0]):v for k,v in self.all_dicts['ts2id'].items()}
+        times = self.all_dicts['tsstr2id'].keys()
+        rels = self.all_dicts['rel2id'].keys()
+        self.split = split
+        
+        #This is hard supervision
+        #args: given TKG, whether to corrupt hard, and how to use the reitrieved timestmaps
+        self.data = retrieve_times(args.tkg_file, args.dataset_name, self.data, args.corrupt_hard, args.fuse)
+        
+        
+        self.data = self.addEntityAnnotation(self.data)
+        self.num_total_entities = len(self.all_dicts['ent2id'])
+        self.num_total_times = len(self.all_dicts['ts2id'])
+        self.padding_idx = self.num_total_entities + self.num_total_times  # padding id for embedding of ent/time
+        self.answer_vec_size = self.num_total_entities + self.num_total_times
+          
+        self.prepared_data = self.prepare_data2(self.data)
+
+    def __len__(self):
+        return len(self.data)
+
+    def getEntityTimeTextIds(self, question, pp_id=0):
+        keyword_dict = question['keyword_dicts'][pp_id]
+        keyword_id_dict = question['annotation']  # this does not depend on paraphrase
+        output_text = []
+        output_ids = []
+        entity_time_keywords = set(['head', 'tail', 'time', 'event_head', 'time1', 'time2'])
+        
+        #print(keyword_dict, keyword_id_dict)
+        for keyword, value in keyword_dict.items():
+            if keyword in entity_time_keywords:
+                wd_id_or_time = keyword_id_dict[keyword]
+                output_text.append(value)
+                output_ids.append(wd_id_or_time)
+                
+        #print(output_text, output_ids)
+        return output_text, output_ids
+
+    def get_entity_aware_tokenization(self, nl_question, ent_times, ent_times_ids):
+        # what we want finally is that after proper tokenization
+        # of nl question, we know which indices are beginning tokens
+        # of entities and times in the question
+        index_et_pairs = []
+        index_et_text_pairs = []
+        for e_text, e_id in zip(ent_times, ent_times_ids):
+            location = nl_question.find(e_text)
+            pair = (location, e_id)
+            index_et_pairs.append(pair)
+            pair = (location, e_text)
+            index_et_text_pairs.append(pair)
+        index_et_pairs.sort()
+        index_et_text_pairs.sort()
+        my_tokenized_question = []
+        start_index = 0
+        arr = []
+        for pair, pair_id in zip(index_et_text_pairs, index_et_pairs):
+            end_index = pair[0]
+            if nl_question[start_index: end_index] != '':
+                my_tokenized_question.append(nl_question[start_index: end_index])
+                arr.append(self.padding_idx)
+            start_index = end_index
+            end_index = start_index + len(pair[1])
+            # todo: assuming entity name can't be blank
+            # my_tokenized_question.append(nl_question[start_index: end_index])
+            my_tokenized_question.append(self.tokenizer.mask_token)
+            matrix_id = self.textToEntTimeId(pair_id[1])  # get id in embedding matrix
+            arr.append(matrix_id)
+            start_index = end_index
+        if nl_question[start_index:] != '':
+            my_tokenized_question.append(nl_question[start_index:])
+            arr.append(self.padding_idx)
+
+        tokenized, valid_ids = self.tokenize(my_tokenized_question)
+        entity_time_final = []
+        index = 0
+        for vid in valid_ids:
+            if vid == 0:
+                entity_time_final.append(self.padding_idx)
+            else:
+                entity_time_final.append(arr[index])
+                index += 1
+        entity_mask = []  # want 0 if entity, 1 if not, since will multiply this later with word embedding
+        for x in entity_time_final:
+            if x == self.padding_idx:
+                entity_mask.append(1.)
+            else:
+                entity_mask.append(0.)
+                
+        #print(entity_time_final)
+        return tokenized, entity_time_final, entity_mask
+
+    def prepare_data2(self, data):
+        # we want to prepare answers lists for each question
+        # then at batch prep time, we just stack these
+        # and use scatter
+        heads = []
+        times = []
+        start_times = []
+        end_times = []
+        tails = []
+        tails2 = []
+        question_text = []
+        tokenized_question = []
+        entity_time_ids_tokenized_question = []
+        entity_mask_tokenized_question = []
+        pp_id = 0
+        num_total_entities = len(self.all_dicts['ent2id'])
+        answers_arr = []
+        for question in tqdm(data):
+            # randomly sample pp
+            # in test there is only 1 pp, so always pp_id=0
+            # TODO: this random is causing assertion bug later on
+            # pp_id = random.randint(0, len(question['paraphrases']) - 1)
+            pp_id = 0
+            nl_question = question['paraphrases'][pp_id]
+            q_text = nl_question
+            et_text, et_ids = self.getEntityTimeTextIds(question, pp_id)
+
+            entities_list_with_locations = self.getEntitiesLocations(question)
+            entities_list_with_locations.sort()
+            entities = [id for location, id in
+                        entities_list_with_locations]  # ordering necessary otherwise set->list conversion causes randomness
+            head = entities[0]  # take an entity
+            if len(entities) > 1:
+                tail = entities[1]
+                if len(entities) > 2:
+                    tail2 = entities[2]
+                else:
+                    tail2 = tail
+            else:
+                tail = entities[0]
+                tail2 = tail
+            times_in_question = question['times']
+            if len(times_in_question) > 0:
+                time = self.timesToIds(times_in_question)[0]  # take a time. if no time then 0
+                start_time = time
+                end_time = time
+                # exit(0)
+            else:
+                time = 0
+                #check for retrieved timestmaps
+                if len(question['fact']) > 0:
+                    ts = []
+                    #add all timestmaps and sort
+                    for f in question['fact']:
+                        for t in range(int(f[0]), int(f[1])+1):
+                            ts.append(t)
+                    ts = sorted(ts)
+                    sorted_times = self.timesToIds(ts)
+                    
+                    try:
+                        start_time = sorted_times[0]   # for random random.choice(sorted_times)
+                    except:
+                        start_time = 0
+                    try:
+                        end_time = sorted_times[-1]
+                    except:
+                        end_time = 0
+                else:
+                    start_time = 0
+                    end_time = 0
+
+                # print('No time in qn!')
+
+
+            time += num_total_entities
+
+            heads.append(head)
+            times.append(time)
+            start_times.append(start_time)
+            end_times.append(end_time)
+            tails.append(tail)
+            tails2.append(tail2)
+
+            tokenized, entity_time_final, entity_mask = self.get_entity_aware_tokenization(nl_question, et_text, et_ids)
+            assert len(tokenized) == len(entity_time_final)
+            question_text.append(nl_question)
+            tokenized_question.append(self.tokenizer.convert_tokens_to_ids(tokenized))
+            entity_mask_tokenized_question.append(entity_mask)
+            entity_time_ids_tokenized_question.append(entity_time_final)
+            if question['answer_type'] == 'entity':
+                answers = self.entitiesToIds(question['answers'])
+            else:
+                # adding num_total_entities to each time id
+                answers = [x + num_total_entities for x in self.timesToIds(question['answers'])]
+            answers_arr.append(answers)
+        return {'question_text': question_text,
+                'tokenized_question': tokenized_question,
+                'entity_time_ids': entity_time_ids_tokenized_question,
+                'entity_mask': entity_mask_tokenized_question,
+                'head': heads,
+                'tail': tails,
+                'time': times,
+                'start_time': start_times,
+                'end_time': end_times,
+                'tail2': tails2,
+                'answers_arr': answers_arr}
+
+    # tokenization function taken from NER code
+    def tokenize(self, words):
+        """ tokenize input"""
+        tokens = []
+        valid_positions = []
+        tokens.append(self.tokenizer.cls_token)
+        valid_positions.append(0)
+        for i, word in enumerate(words):
+            token = self.tokenizer.tokenize(word)
+            tokens.extend(token)
+            for i in range(len(token)):
+                if i == 0:
+                    valid_positions.append(1)
+                else:
+                    valid_positions.append(0)
+        tokens.append(self.tokenizer.sep_token)
+        valid_positions.append(0)
+        return tokens, valid_positions
+
+    def __getitem__(self, index):
+        data = self.prepared_data
+        question_text = data['question_text'][index]
+        entity_time_ids = np.array(data['entity_time_ids'][index], dtype=np.long)
+        answers_arr = data['answers_arr'][index]
+        answers_single = random.choice(answers_arr)
+        # answers_khot = self.toOneHot(answers_arr, self.answer_vec_size)
+        tokenized_question = data['tokenized_question'][index]
+        entity_mask = data['entity_mask'][index]
+        head = data['head'][index]
+        tail = data['tail'][index]
+        tail2 = data['tail2'][index]
+        time = data['time'][index]
+        start_time = data['start_time'][index]
+        end_time = data['end_time'][index]
+
+        return question_text, tokenized_question, entity_time_ids, entity_mask, head, tail, time, start_time, end_time, tail2, answers_single
+
+    def pad_for_batch(self, to_pad, padding_val, dtype=np.long):
+        padded = np.ones([len(to_pad), len(max(to_pad, key=lambda x: len(x)))], dtype=dtype) * padding_val
+        for i, j in enumerate(to_pad):
+            padded[i][0:len(j)] = j
+        return padded
+
+    # do this before padding for batch
+    def get_attention_mask(self, tokenized):
+        # first make zeros array of appropriate size
+        mask = np.zeros([len(tokenized), len(max(tokenized, key=lambda x: len(x)))], dtype=np.long)
+        # now set ones everywhere needed
+        for i, j in enumerate(tokenized):
+            mask[i][0:len(j)] = np.ones(len(j), dtype=np.long)
+        return mask
+
+    def _collate_fn(self, items):
+        # please don't tokenize again
+        # b = self.tokenizer(batch_sentences, padding=True, truncation=False, return_tensors="pt")
+
+        tokenized_questions = [item[1] for item in items]
+        attention_mask = torch.from_numpy(self.get_attention_mask(tokenized_questions))
+        input_ids = torch.from_numpy(self.pad_for_batch(tokenized_questions, self.tokenizer.pad_token_id, np.long))
+
+        entity_time_ids_list = [item[2] for item in items]
+        entity_time_ids_padded = self.pad_for_batch(entity_time_ids_list, self.padding_idx, np.long)
+        entity_time_ids_padded = torch.from_numpy(entity_time_ids_padded)
+
+        entity_mask = [item[3] for item in items]  # 0 if entity, 1 if not
+        entity_mask_padded = self.pad_for_batch(entity_mask, 1.0,
+                                                np.float32)  # doesnt matter probably cuz attention mask will be used. maybe pad with 1?
+        entity_mask_padded = torch.from_numpy(entity_mask_padded)
+        # can make foll mask in forward function using attention mask
+        # entity_time_ids_padded_mask = ~(attention_mask.bool())
+
+        heads = torch.from_numpy(np.array([item[4] for item in items]))
+        tails = torch.from_numpy(np.array([item[5] for item in items]))
+        times = torch.from_numpy(np.array([item[6] for item in items]))
+        start_times = torch.from_numpy(np.array([item[7] for item in items]))
+        end_times = torch.from_numpy(np.array([item[8] for item in items]))
+        
+        tails2 = torch.from_numpy(np.array([item[9] for item in items]))
+
+        # answers_khot = torch.stack([item[4] for item in items])
+        answers_single = torch.from_numpy(np.array([item[10] for item in items]))
+
+        return input_ids, attention_mask, entity_time_ids_padded, entity_mask_padded, heads, tails, times, start_times, end_times, tails2, answers_single
+
 
 
 
